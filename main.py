5f002c986ca2558944b181d12d3b06e4c3193371
Jonathan Chng <jonathanchngyk@gmail.com>
2023-04-14 20:28:12 +0800

    SQLite ImporT

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import sqlite3
 
 app = Flask(__name__)
 db = sqlite3.connect("books-collection.db")
-
+cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
 all_books = []
 
 
